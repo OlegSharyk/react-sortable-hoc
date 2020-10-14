00a07326d7b1603913b209d1d07fc62225263b12
Oleg Sharyk <docshark@gmail.com>
2020-10-14 21:52:20 +0300

    Update react-sortable-hoc.esm.js

--- dist/react-sortable-hoc.esm.js
+++ dist/react-sortable-hoc.esm.js
@@ -89,7 +89,7 @@ function arrayMove(array, from, to) {
   if (process.env.NODE_ENV !== 'production') {
     if (typeof console !== 'undefined') {
       console.warn(
-        "Deprecation warning: arrayMove will no longer be exported by 'react-sortable-hoc' in the next major release. Please install the `array-move` package locally instead. https://www.npmjs.com/package/array-move",
+        "Deprecation warning: arrayMove will no longer be exported by 'react-sortable-hoc' in the next major release. Please install the `array-move` package locally instead. https://www.npmjs.com/package/array-move"
       );
     }
   }
